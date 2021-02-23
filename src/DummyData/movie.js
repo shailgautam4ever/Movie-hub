7ca02f834122a7475c84df1d56f94e78772d43a6
Shail <rayskcoc@gmail.com>
2021-02-24 03:24:55 +0530

    modified dummy data

---
+++ src/DummyData/movie.js
@@ -0,0 +1,67 @@
+const allMovies = [
+  {
+    id: 1,
+    name: "Justice League",
+    genre: "Action, Thriller",
+    rating: 4,
+    img:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQr8TLcGyFZmLsJSNZ3M_8CO2M9YbnucnpGTs6u2erH-SveV1O2",
+  },
+  {
+    id: 2,
+    name: "Godzilla vs Kong",
+    genre: "Action, Thriller",
+    rating: 5,
+    img:
+      "https://m.media-amazon.com/images/M/MV5BZmYzMzU4NjctNDI0Mi00MGExLWI3ZDQtYzQzYThmYzc2ZmNjXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
+  },
+  {
+    id: 3,
+    name: "Croods 3",
+    genre: "Anime, Comedy",
+    rating: 5,
+    img:
+      "https://m.media-amazon.com/images/M/MV5BNGU1NGNiYzYtMTQ2Ni00M2ZlLTg0N2QtMDFhMzNjNzcyMGYyXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
+  },
+  {
+    id: 4,
+    name: "Tom & Jerry",
+    genre: "Anime, Comedy",
+    rating: 5,
+    img:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQT6SAhNZkTMzGWUO9uWpNUZYLh979WmMxf-CIRGCGK9UfNfu9M",
+  },
+  {
+    id: 5,
+    name: "Captain Marvel",
+    genre: "Action",
+    rating: 3,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/810AwpOyZfL._RI_.jpg",
+  },
+  {
+    id: 6,
+    name: "Wonder Woman 1984",
+    genre: "Action, Fantasy",
+    rating: 5,
+    img:
+      "https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/wonder-woman-1984-cinepolis-promotion-show-et00303447-25-12-2020-04-52-05.jpg",
+  },
+  {
+    id: 7,
+    name: "The Three Musketeers",
+    genre: "Action Adventure",
+    rating: 5,
+    img:
+      "https://upload.wikimedia.org/wikipedia/en/5/52/Barbie_and_the_Three_Musketeers.jpg",
+  },
+  {
+    id: 8,
+    name: "The Three Musketeers",
+    genre: "Action Adventure",
+    rating: 5,
+    img: "https://owlswellblog.files.wordpress.com/2013/11/frozen-a4.jpg",
+  },
+];
+
+export default allMovies;
