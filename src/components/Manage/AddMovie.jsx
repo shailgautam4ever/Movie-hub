c9e66deb0d90031fe7abf545b668c8d9901df8b2
Shail <rayskcoc@gmail.com>
2021-02-24 03:28:37 +0530

    create new movie form added

--- src/components/Manage/AddMovie.jsx
+++ src/components/Manage/AddMovie.jsx
@@ -1,23 +1,72 @@
-import React from 'react';
-export default class AddMovie extends React.Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return(
-            <>
-            <div className='Add-container'>
-                
-                <span>Add New Movie</span>
-                <div>
-                <span>Name</span>
-                <input type='text'></input>
-                </div>
-                <div>
-                <span>Genre</span>
-                </div>
+import React from "react";
+import "./AddMovie.css";
+import "../Home/Home.css";
+import movies from "../../DummyData/movie";
+
+const form = [
+  {
+    id: "name",
+    label: "Name",
+  },
+  {
+    id: "genre",
+    label: "Genre",
+  },
+  {
+    id: "rating",
+    label: "Rating",
+  },
+  {
+    id: "image",
+    label: "Image",
+  },
+  {
+    id: "chooseFile",
+    label: "Choose File",
+    type: "file",
+  },
+];
+export default class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  handleOnChange = (e, id) => {
+    const value = e.target.value;
+    this.setState({
+      [id]: value,
+    });
+    console.log(this.state);
+  };
+  handleOnAdd = () => {
+    movies.push(this.state);
+  };
+  render() {
+    return (
+      <>
+        <div className="Add-container fd-c">
+          <div className="df fd-c jc-sb wrapper">
+            <span className="title">Add New Movie</span>
+            {form.map(({ label, type, id }) => (
+              <div className="df ai-b jc-sb">
+                <span>{label}</span>
+                <input
+                  onChange={(e) => this.handleOnChange(e, id)}
+                  className="search-bar"
+                  type={type ? type : "text"}
+                ></input>
+              </div>
+            ))}
+
+            <div>
+              <button onClick={this.handleOnAdd} className="custom-button">
+                Add
+              </button>
+              <button className="custom-button">Cancel</button>
             </div>
-            </>
-        )
-    }
+          </div>
+        </div>
+      </>
+    );
+  }
 }
