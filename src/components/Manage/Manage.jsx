3f5b3c66f774f1e58fb21f81b5ea079abd6a901f
Shail <rayskcoc@gmail.com>
2021-02-26 03:36:16 +0530

    raised event for deleting

--- src/components/Manage/Manage.jsx
+++ src/components/Manage/Manage.jsx
@@ -1,42 +1,28 @@
 import React from "react";
-import movies from "../../DummyData/movie";
 import "./Manage.css";
 import "../../Movie/Movie.css";
 import { Link } from "react-router-dom";
 export default class Manage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movie: movies,
-    };
-  }
-  handleOnDelete = (e, id) => {
-    const { movie } = this.state;
-    const index = movie.findIndex((v) => v.id === id);
-    movie.splice(index, 1);
-    // movies = movie;
-    this.setState({ movie });
-  };
   render() {
-    const { movie } = this.state;
+    const { movies, handleOnDelete } = this.props;
     return (
       <>
         <div className="df fd-c p-10">
           <Link to="/manage/AddMovie">
             <button className="custom-button">Add Movie</button>
           </Link>
-          {movie.map((v, i) => (
+          {movies.map((v, i) => (
             <div className="df jc-sb show-table">
               <p>{v.name}</p>
               <div>
                 <button className="custom-button">Update</button>
                 <button
                   className="custom-button"
-                  onClick={(e) => this.handleOnDelete(e, v.id)}
+                  onClick={(e) => handleOnDelete(e, v.id)}
                 >
                   Delete
                 </button>
-                <Link to={`movie/${i}`}>
+                <Link to={`/movie/${i}`}>
                   <button className="custom-button">View</button>
                 </Link>
               </div>
